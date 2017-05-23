d6c0fae11682403dacc2965c2e1c73e423f03085
GregMercereau <greg.mercereau@orange.fr>
2017-05-23 12:05:51 +0200

    Connexion en tant qu'admin, page dédiée, hachage sha1 des mdp

--- KIWI/Controleur/cible.php
+++ KIWI/Controleur/cible.php
@@ -6,18 +6,23 @@ require('../Modele/utilisateurs.php');
 if(!empty($_POST['pseudo']) && !empty($_POST['mdp'])){ // L'utilisateur a rempli tous les champs du formulaire
     $nom = $_POST['pseudo'];
 
-    $result = mdp($db,$nom);
+    $resultc = mdp($db,$nom);
+    $resulta = mdpadmin($db,$nom);
 
-    if(md5($_POST['mdp'])!=$result['mot_de_passe']){ // Le mot de passe entré ne correspond pas à celui stocké dans la base de données
+    if(sha1($_POST['mdp'])!=$resultc['mot_de_passe'] && sha1($_POST['mdp'])!=$resulta['mot_de_passe']){ // Le mot de passe entré ne correspond pas à celui stocké dans la base de données
         $erreur = "Nom ou mot de passe incorrect";
         echo $erreur;
         include '../Vue/PageAccueil.php';
-
-
     }
 
-    else { // mot de passe correct, on affiche la page d'accueil
-        $_SESSION = $result;
+    elseif (sha1($_POST['mdp'])==$resulta['mot_de_passe']) {     //L'utilisateur est un admin
+        $_SESSION = $resulta;
+        echo "Bienvenue M.".$_SESSION['nom'];
+        include '../Vue/admin.php';
+    }
+
+    else { // mot de passe client correct, on affiche la page d'accueil
+        $_SESSION = $resultc;
         echo "Bienvenue M.".$_SESSION['nom'];
         include '../Vue/PageAccueil.php';
     }
--- KIWI/Modele/utilisateurs.php
+++ KIWI/Modele/utilisateurs.php
@@ -8,3 +8,10 @@ function mdp($db,$identifiant){
     $result = $reponse->fetch(PDO::FETCH_ASSOC);
     return $result;
 }
+
+function mdpadmin($db,$identifiant) {
+    $reponse = $db->prepare('SELECT nom,mot_de_passe FROM administrateur WHERE nom="'.$identifiant.'"');
+    $reponse->execute();
+    $result = $reponse->fetch(PDO::FETCH_ASSOC);
+    return $result;
+}
