dc7055172a6340380383f7e84b49e3585a7288e9
GregMercereau <greg.mercereau@orange.fr>
2017-05-29 16:11:32 +0200

    Debug

--- KIWI/Controleur/cible.php
+++ KIWI/Controleur/cible.php
@@ -9,13 +9,19 @@ if(!empty($_POST['pseudo']) && !empty($_POST['mdp'])){ // L'utilisateur a rempli
     $resultc = mdp($db,$nom);
     $resulta = mdpadmin($db,$nom);
 
-    if(sha1($_POST['mdp'])!=$resultc['mot_de_passe'] && sha1($_POST['mdp'])!=$resulta['mot_de_passe']){ // Le mot de passe entré ne correspond pas à celui stocké dans la base de données
-        $erreur = "Nom ou mot de passe incorrect";
+    if(md5($_POST['mdp'])!= $resultc['mot_de_passe']){ // Le mot de passe entré ne correspond pas à celui stocké dans la base de données client
+        $erreur = "Nom ou mot de passe client incorrect";
         echo $erreur;
         include '../Vue/PageAccueil.php';
     }
 
-    elseif (sha1($_POST['mdp'])==$resulta['mot_de_passe']) {     //L'utilisateur est un admin
+    elseif(md5($_POST['mdp'])!= $resulta['mot_de_passe']) { //Mauvais mdp version admin
+        $erreur = "Nom ou mot de passe admin incorrect";
+        echo $erreur;
+        include '../Vue/PageAccueil.php';
+    }
+
+    elseif (md5($_POST['mdp']) == $resulta['mot_de_passe']) {     //L'utilisateur est un admin
         $_SESSION = $resulta;
         echo "Bienvenue M.".$_SESSION['nom'];
         include '../Vue/admin.php';
