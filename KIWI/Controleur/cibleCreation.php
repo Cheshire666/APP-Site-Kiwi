b008c7e79570f8c939093f2483723b49c5ab2a53
GregMercereau <greg.mercereau@orange.fr>
2017-06-09 16:04:58 +0200

    Màj du 9/06. Insertion pieces bdd entre autres

--- KIWI/Controleur/cibleCreation.php
+++ KIWI/Controleur/cibleCreation.php
@@ -1,23 +1,12 @@
 <?php
 
 require_once('../Modele/connexion.php');
-require_once ('../Modele/insertion.php');
 
-$id_piece=$_POST['id_piece'];
-$choix_piece=$_POST['choix_piece'];
-$type=$_POST['type'];
-$superficie=$_POST['superficie'];
-$nb_capteur=$_POST['nb_capteur'];
-$id_maison=$_POST['id_maison'];
+if (!empty($_POST['id_piece']) AND !empty($_POST['choix_piece']) AND !empty($_POST['type']) AND !empty($_POST['superficie']) AND !empty($_POST['nb_capteur']) AND !empty($_POST['id_maison'])){
 
-if (!empty($_POST['id_piece'])&&($_POST['choix_piece'])&&($_POST['type'])&&($_POST['superficie'])&&($_POST['nb_capteur'])&&($_POST['id_maison'])){
-    insertion_piece($db);
-    echo 'Vous avez bien ajouté:';
-    $reponse = $db->prepare('SELECT * FROM piece');
-    $reponse->execute();
-    $donnees = $reponse->fetch(PDO::FETCH_ASSOC);
-    var_dump($donnees);
-
+    $db->exec('INSERT INTO piece VALUES("'.$_POST['id_piece'].'", "'.$_POST['choix_piece'].'", "'.$_POST['type'].'", "'.$_POST['superficie'].'", "'.$_POST['nb_capteur'].'", "'.$_POST['id_maison'].'")') or die(print_r($db->errorInfo()));
+    echo '<script language="JavaScript">window.alert("Félicitations, le fomulaire a été ajouté dans la base de données !")</script>';
+    include '../Vue/Creer.php';
 }
 
 
--- KIWI/Vue/editProfil.php
+++ KIWI/Vue/editProfil.php
@@ -10,29 +10,29 @@ require 'client.php';
     <link rel="stylesheet" href="../Style/editProfil.css" type="text/css">
 </head>
 
-<body onLoad="GereControle(document.getElementsByName('header'))">
+<body>
 
-<?php echo '<h1>Nom : '.$_SESSION['nom']; ?>
+<?php echo '<h1>Nom : '.$_SESSION['nom']; ?><br><br>
 
     <script type="text/javascript">
         function bascule(id)
         {
             if (document.getElementById(id).style.visibility === "hidden")
                 document.getElementById(id).style.visibility = "visible";
-            else	document.getElementById(id).style.visibility = "hidden";
+            else document.getElementById(id).style.visibility = "hidden";
         }
 
     </script>
 <div id="bouton" onclick="bascule('header');">Modifier mon mot de passe</div>
 
-<div id="header">
+<div id="header" onload="visibility = 'hidden'">
     <form method=post action="../Controleur/modifmdp.php">
         <ul>
             <label>Ancien mot de passe</label>
             <input type="password" name="amdp" id="amdp"><br>
             <label>Nouveau mot de passe</label>
             <input type="password" name="nmdp" id="nmdp"><br>
-            <input type="submit" name="submit" value="Modifier" class="btn">
+            <input type="submit" name="submit" value="Modifier" style="background-color: #3F95AD; border-radius: 8px; font-family: 'Dosis Medium'; font-size: large">
         </ul>
     </form>
 </div>
