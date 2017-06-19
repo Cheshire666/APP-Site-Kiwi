9d6ca2fdd849449c1de02439e98f4c66a612a449
Juliette Narcy <juliette.narcy78@gmail.com>
2017-06-19 15:48:52 +0200

    Bouton on/off

--- KIWI/Controleur/cibleOnOff.php
+++ KIWI/Controleur/cibleOnOff.php
@@ -3,7 +3,7 @@ require_once('../Modele/connexion.php');
 
 
 $check = "-";
-foreach($_POST["onoff"] as $checkoptions){
+foreach($_POST["onoff[]"] as $checkoptions){
     $check .= $checkoptions."-";
 }
 echo $check;
